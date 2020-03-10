f2a9f58246c381b88f1483684f4546bd082b2d7e
andry <andry46lviv@gmail.com>
2020-03-10 17:09:53 +0200

    p

--- src/main/java/com/examle/tempmon/GraphController.java
+++ src/main/java/com/examle/tempmon/GraphController.java
@@ -3,7 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//main controller
+//main controller//
 
 @Controller
 public class GraphController {
