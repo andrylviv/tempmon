e9626e049b4516c5594eccc012a234788b5ecf2e
andry <andry46lviv@gmail.com>
2020-04-28 10:28:11 +0300

    m

--- src/main/java/com/example/tempmon/GreetingController.java
+++ src/main/java/com/example/tempmon/GreetingController.java
@@ -1,4 +1,4 @@
-package com.examle.tempmon;
+package com.example.tempmon;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/example/tempmon/Temperature.java
+++ src/main/java/com/example/tempmon/Temperature.java
@@ -4,16 +4,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class Temperature {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
 
-    private String name;
+    private LocalDateTime date_time;
 
-    private String email;
+    private Double temperature;
 
     public Integer getId() {
         return id;
@@ -23,19 +25,19 @@ public class Temperature {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public LocalDateTime getDateTime() {
+        return date_time;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setDateTime(LocalDateTime date_time) {
+        this.date_time = date_time;
     }
 
-    public String getEmail() {
-        return email;
+    public Double getTemperature() {
+        return temperature;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
     }
 }
